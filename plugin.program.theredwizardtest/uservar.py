5e0338c3c553c65f8e319c42bbad2fe0de036f79
The-Red-Wizard <46077626+The-Red-Wizard@users.noreply.github.com>
2026-04-06 07:44:20 +0100

    Add files via upload

---
+++ plugin.program.theredwizardtest/uservar.py
@@ -0,0 +1,18 @@
+'''#####-----Build File-----#####'''
+buildfile = 'https://repo.redwizard.xyz/wizard/redwizard-test-builds.xml'
+
+'''#####-----Videos File-----#####'''
+videos_url = 'https://repo.redwizard.xyz/wizard/rumble/rw-youtube.txt'
+
+'''#####-----Notification File-----#####'''
+notify_url  = 'https://repo.redwizard.xyz/wizard/notifications/notify-test.txt'
+
+'''#####-----Changelog Directory-----#####'''
+changelog_dir  = 'https://repo.redwizard.xyz/wizard/changelogs/'
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.example']
+
+#Change Build Names
+BUILDS = [{'Old Build': "Old Build_1 Here", 'New Build': "New Build_1 Here"},
+          {'Old Build': "Old Build_2 Here", 'New Build': "New Build_2 Here"}]
